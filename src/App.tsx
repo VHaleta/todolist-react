845e630c6b0f353ffea292cd4e87428fec513c51
VHaleta <vhaleta.official@gmail.com>
2024-01-17 14:35:30 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import TaskList from "./Components/TaskList";
+import "./App.css";
+
+type AppProps = {};
+
+export type Task = {
+  id: number,
+  text: string,
+  completed: boolean
+}
+
+const defaultTasks: Array<Task> = [
+  {id: 0, text: "Some task", completed: false},
+  {id: 1, text: "Some task", completed: true}
+];
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <div>
+      <TaskList/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/TaskList.tsx
@@ -0,0 +1,82 @@
+import TaskItem from "./TaskItem";
+import { type Task } from "../App";
+import { useState } from "react";
+
+type TaskListProps = {
+  tasks?: Array<Task>
+}
+
+const TaskList = ({ tasks }: TaskListProps) => {
+  const defaultTasks = []
+  if (tasks != undefined)
+    defaultTasks.push(...tasks)
+  else{
+    defaultTasks.push(...JSON.parse(localStorage.getItem("tasks")?? '{\"tasks\": []}').tasks)
+  }
+
+  const [tasksState, setTasksState] = useState(defaultTasks)
+  const [inputState, setInputState] = useState("")
+
+  const loadTasksToStorage = (newTasks: Array<Task>) => {
+    localStorage.setItem("tasks", JSON.stringify({ tasks: [...newTasks] }))
+  }
+
+  const onRemove = (id: number) => {
+    const copy = [...tasksState]
+    const removeIndex = copy.findIndex((task) => task.id == id)
+    copy.splice(removeIndex, 1)
+    setTasksState(copy)
+    loadTasksToStorage(copy)
+  };
+
+  const onToggle = (id: number) => {
+    const copy = [...tasksState];
+    const updateIndex = copy.findIndex((task) => task.id == id)
+    copy[updateIndex].completed = !copy[updateIndex].completed
+    setTasksState(copy)
+    loadTasksToStorage(copy)
+  };
+
+  const onAddClick = () => {
+    if (inputState == "") return;
+    const isEmpty = tasksState.length <= 0
+    const copy = [...tasksState, { id: (isEmpty) ? 0 : tasksState[tasksState.length - 1].id + 1, text: inputState, completed: false }]
+    console.log(copy)
+    setTasksState(copy)
+    setInputState("")
+    loadTasksToStorage(copy)
+  };
+
+  const isEmpty: boolean = tasksState.length === 0;
+  return (
+    <div className="task-list">
+      <div className="task-list-title">Task list</div>
+      <div className="add-task-form">
+        <input
+          className="add-task-input"
+          onChange={(e) => {
+            setInputState(e.target.value);
+          }}
+          value={inputState}
+        ></input>
+        <button className="add-task-button" onClick={() => onAddClick()}>
+          Add
+        </button>
+      </div>
+      {isEmpty ? (
+        <div className="task-list-empty">No tasks to display</div>
+      ) : (
+        tasksState.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          ></TaskItem>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default TaskList
